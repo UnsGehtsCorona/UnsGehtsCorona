84bcd86ca5cbba6ff5cc35bb93f0f729ebfa89b0
PerhapsSomeone <ma2003br@gmail.com>
2020-03-22 23:52:58 +0100

    Extend node server to serve new files

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
+var express = require("express");
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get("/cursor.png", function (req, res) {
-    res.sendFile(__dirname + '/cursor.png');
-});
 
 io.on('connection', function(socket){
     console.log('a user connected');
@@ -58,6 +51,8 @@ function distributeCursorPositions() {
 }
 
 setInterval(distributeCursorPositions, 500);
+
+app.use(express.static('ResponsiveCanvas2'));
 
 http.listen(3333, function() {
     console.log('listening on *:3333');
