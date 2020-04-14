2270316f163c1f4ff1b53ecb3dfe087b52194a28
Benedict Burckhart <bb@stilgrenze.de>
2020-04-14 13:08:03 +0200

    Cursor API Connection readded

---
+++ ResponsiveCanvas2/cursors.js
@@ -0,0 +1,18 @@
+let pageIdentifier = "corona_page_" + questionNumber;
+let socket = io();
+
+socket.on("connect", (socket) => {
+    setInterval(sendMousePosition, 100);
+});
+
+socket.on("positionsDataPack", (coords) => {
+    drawCursors(coords);
+});
+
+function sendMousePosition() {
+    socket.emit("cursorPosition", {
+        "x": mouseX,
+        "y": mouseY,
+        "id": pageIdentifier
+    });
+}
